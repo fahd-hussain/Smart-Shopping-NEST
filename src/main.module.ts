03de883f1001ce0ff9abc54ff004e78f453d564e
Fahad Hussain <fahd__hussain@hotmail.com>
2021-08-02 01:04:10 +0500

    HTTP Error filtration

--- src/main.module.ts
+++ src/main.module.ts
@@ -3,8 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import ormConfig from './config/ormconfig';
 import { AuthModule } from './auth-module/auth.module';
 import { AppModule } from './app-module/app.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/http-error.filter';
 
 @Module({
   imports: [TypeOrmModule.forRoot(ormConfig), AuthModule, AppModule],
+  providers: [{
+    provide: APP_FILTER,
+    useClass: HttpErrorFilter
+  }]
 })
 export class MainModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import ormConfig from './config/ormconfig';
 import { MainModule } from './main.module';
-require('dotenv').config();
 
-// const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
   app.setGlobalPrefix('api/v1/');
-  app.useLogger(['log', 'error', 'warn', 'debug', 'verbose']);
 
   await app.listen(3000);
 }
